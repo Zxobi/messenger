2be6bd94faf37dd9026dcfda3d419b2c2032b364
Daniil Videneev <danyavid@gmail.com>
2024-04-21 23:06:56 +0300

    Fix tests format & use parallel

--- server/internal/storage/auth/inmem/immem_test.go
+++ server/internal/storage/auth/inmem/immem_test.go
@@ -1,8 +1,10 @@
-package inmem
+package inmem_test
 
 import (
+	"fmt"
 	"github.com/dvid-messanger/internal/domain/model"
 	"github.com/dvid-messanger/internal/storage/auth"
+	"github.com/dvid-messanger/internal/storage/auth/inmem"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -12,78 +14,58 @@ import (
 )
 
 func TestAuthStorage_Save(t *testing.T) {
-	storage := New()
+	op := "TestAuthStorage_Save"
+	t.Parallel()
 
 	for i := 1; i <= 10; i++ {
-		expected := genCreds("test-mail"+strconv.Itoa(i), "test-password")
+		t.Run(fmt.Sprintf("%s i = %d", op, i), func(t *testing.T) {
+			t.Parallel()
 
-		actualR, err := storage.Save(context.Background(), expected.Id, expected.Email, expected.PassHash)
+			storage := inmem.New()
+			expected := genCreds("test-mail"+strconv.Itoa(i), "test-password")
+
+			actualR, err := storage.Save(context.Background(), expected.Id, expected.Email, expected.PassHash)
+			require.NoError(t, err, "save should not error")
+
+			assert.Equal(t, *expected, actualR, "returned creds match expected")
+
+			user, err := storage.User(context.Background(), expected.Email)
+			require.NoError(t, err, "user should not error")
+			assert.Equal(t, *expected, user, "saved creds match expected")
+		})
+	}
+
+	t.Run(fmt.Sprintf("%s_EmailExistsReturnError", op), func(t *testing.T) {
+		t.Parallel()
+
+		storage := inmem.New()
+
+		creds1 := genCreds("test-mail1", "test-password1")
+		creds2 := genCreds("test-mail2", "test-password2")
+
+		_, err := storage.Save(context.Background(), creds1.Id, creds1.Email, creds1.PassHash)
 		require.NoError(t, err, "save should not error")
 
-		assert.ElementsMatch(t, expected.Id, actualR.Id, "returned id match expected")
-		assert.Equal(t, expected.Email, actualR.Email, "returned email match expected")
-		assert.Equal(t, expected.PassHash, actualR.PassHash, "returned passHash match expected")
-
-		assert.Len(t, storage.hash, i, "hash len correct")
-
-		actualS, ok := storage.hash[expected.Email]
-		assert.True(t, ok, "hash contains creds")
-		assert.ElementsMatch(t, expected.Id, actualS.Id, "hash id match expected")
-		assert.Equal(t, expected.Email, actualS.Email, "hash email match expected")
-		assert.Equal(t, expected.PassHash, actualS.PassHash, "hash passHash match expected")
-	}
-}
-
-func TestAuthStorage_Save_SameEmailError(t *testing.T) {
-	storage := New()
-
-	creds1 := genCreds("test-mail", "test-password")
-	_, err := storage.Save(context.Background(), creds1.Id, creds1.Email, creds1.PassHash)
-	require.NoError(t, err, "save should not error")
-
-	creds2 := genCreds("test-mail", "test-password2")
-	_, err = storage.Save(context.Background(), creds2.Id, creds1.Email, creds2.PassHash)
-	if assert.Error(t, err, "save with same email should error") {
+		_, err = storage.Save(context.Background(), creds2.Id, creds1.Email, creds2.PassHash)
+		require.Error(t, err, "save existing same email should error")
 		assert.ErrorIs(t, err, auth.ErrUserExists, "error type correct")
-	}
-}
+	})
 
-func TestAuthStorage_Save_SameIdError(t *testing.T) {
-	storage := New()
+	t.Run(fmt.Sprintf("%s_IdExistsReturnError", op), func(t *testing.T) {
+		t.Parallel()
 
-	creds1 := genCreds("test-mail", "test-password")
-	_, err := storage.Save(context.Background(), creds1.Id, creds1.Email, creds1.PassHash)
-	require.NoError(t, err, "save should not error")
+		storage := inmem.New()
 
-	creds2 := genCreds("test-mail2", "test-password2")
-	_, err = storage.Save(context.Background(), creds1.Id, creds2.Email, creds2.PassHash)
-	if assert.Error(t, err, "save with same id should error") {
+		creds1 := genCreds("test-mail1", "test-password1")
+		creds2 := genCreds("test-mail2", "test-password2")
+
+		_, err := storage.Save(context.Background(), creds1.Id, creds1.Email, creds1.PassHash)
+		require.NoError(t, err, "save should not error")
+
+		_, err = storage.Save(context.Background(), creds1.Id, creds2.Email, creds2.PassHash)
+		require.Error(t, err, "save with existing id should error")
 		assert.ErrorIs(t, err, auth.ErrUserExists, "error type correct")
-	}
-}
-
-func TestAuthStorage_User(t *testing.T) {
-	storage := New()
-
-	creds1 := genCreds("test-mail", "test-password")
-	creds2 := genCreds("test-mail2", "test-password2")
-	_, err := storage.Save(context.Background(), creds1.Id, creds1.Email, creds1.PassHash)
-	require.NoError(t, err, "save should not error")
-	_, err = storage.Save(context.Background(), creds2.Id, creds2.Email, creds2.PassHash)
-	require.NoError(t, err, "save should not error")
-
-	actual1, err := storage.User(context.Background(), creds1.Email)
-	require.NoError(t, err, "user should not error")
-	actual2, err := storage.User(context.Background(), creds2.Email)
-	require.NoError(t, err, "user should not error")
-
-	assert.ElementsMatch(t, creds1.Id, actual1.Id, "id match expected")
-	assert.Equal(t, creds1.Email, actual1.Email, "email match expected")
-	assert.Equal(t, creds1.PassHash, actual1.PassHash, "passHash match expected")
-
-	assert.ElementsMatch(t, creds2.Id, actual2.Id, "id match expected")
-	assert.Equal(t, creds2.Email, actual2.Email, "email match expected")
-	assert.Equal(t, creds2.PassHash, actual2.PassHash, "passHash match expected")
+	})
 }
 
 func genCreds(email string, pass string) *model.UserCredentials {
--- server/internal/storage/user/inmem/immem_test.go
+++ server/internal/storage/user/inmem/immem_test.go
@@ -1,9 +1,10 @@
-package inmem
+package inmem_test
 
 import (
+	"fmt"
 	"github.com/dvid-messanger/internal/domain/model"
-	"github.com/dvid-messanger/internal/lib/id"
 	"github.com/dvid-messanger/internal/storage/user"
+	"github.com/dvid-messanger/internal/storage/user/inmem"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -13,84 +14,45 @@ import (
 )
 
 func TestUserStorage_Save(t *testing.T) {
-	storage := New()
+	op := "TestUserStorage_Save"
+	t.Parallel()
 
 	for i := 1; i <= 10; i++ {
-		expected := genUser("test-mail"+strconv.Itoa(i), "test-bio")
+		t.Run(fmt.Sprintf("%s i = %d", op, i), func(t *testing.T) {
+			t.Parallel()
 
-		actualR, err := storage.Save(context.Background(), expected.Email, expected.Bio)
+			storage := inmem.New()
+
+			expected := genUser("test-mail"+strconv.Itoa(i), "test-bio")
+
+			actualR, err := storage.Save(context.Background(), expected.Email, expected.Bio)
+			require.NoError(t, err, "save should not error")
+			assert.NotEmpty(t, actualR.Id, "returned id not empty")
+
+			expected.Id = actualR.Id
+			assert.Equal(t, *expected, actualR, "returned user match expected")
+
+			saved, err := storage.User(context.Background(), expected.Id)
+			require.NoError(t, err, "user should not error")
+			assert.Equal(t, *expected, saved)
+		})
+	}
+
+	t.Run(fmt.Sprintf("%s_EmailExistsReturnError", op), func(t *testing.T) {
+		t.Parallel()
+
+		storage := inmem.New()
+
+		user1 := genUser("test-mail1", "test-bio1")
+		user2 := genUser("test-mail2", "test-bio2")
+
+		_, err := storage.Save(context.Background(), user1.Email, user1.Bio)
 		require.NoError(t, err, "save should not error")
 
-		assert.NotEmpty(t, actualR.Id, "returned id not empty")
-		assert.Equal(t, expected.Email, actualR.Email, "returned email match expected")
-		assert.Equal(t, expected.Bio, actualR.Bio, "returned bio match expected")
-
-		assert.Len(t, storage.hash, i, "hash len correct")
-
-		actualS, ok := storage.hash[id.Id(actualR.Id)]
-		assert.True(t, ok, "hash contains user")
-		assert.Equal(t, actualR.Id, actualS.Id, "hash id match returned")
-		assert.Equal(t, expected.Email, actualR.Email, "hash email match expected")
-		assert.Equal(t, expected.Bio, actualS.Bio, "hash bio match expected")
-	}
-}
-
-func TestUserStorage_Save_SameEmailError(t *testing.T) {
-	storage := New()
-
-	user1 := genUser("test-mail", "test-bio")
-	_, err := storage.Save(context.Background(), user1.Email, user1.Bio)
-	require.NoError(t, err, "save should not error")
-
-	user2 := genUser("test-mail", "test-bio2")
-	_, err = storage.Save(context.Background(), user2.Email, user2.Bio)
-	if assert.Error(t, err, "save with same email should error") {
+		_, err = storage.Save(context.Background(), user1.Email, user2.Bio)
+		require.Error(t, err, "save with existing id should error")
 		assert.ErrorIs(t, err, user.ErrUserExists, "error type correct")
-	}
-}
-
-func TestUserStorage_User(t *testing.T) {
-	storage := New()
-
-	user1 := genUser("test-mail", "test-bio")
-	user2 := genUser("test-mail2", "test-bio2")
-	saved1, err := storage.Save(context.Background(), user1.Email, user1.Bio)
-	require.NoError(t, err, "save should not error")
-	saved2, err := storage.Save(context.Background(), user2.Email, user2.Bio)
-	require.NoError(t, err, "save should not error")
-
-	actual1, err := storage.User(context.Background(), saved1.Id)
-	require.NoError(t, err, "user should not error")
-	actual2, err := storage.User(context.Background(), saved2.Id)
-	require.NoError(t, err, "user should not error")
-
-	assert.ElementsMatch(t, saved1.Id, actual1.Id, "id match saved")
-	assert.Equal(t, user1.Email, actual1.Email, "email match expected")
-	assert.Equal(t, user1.Bio, actual1.Bio, "bio match expected")
-
-	assert.ElementsMatch(t, saved2.Id, actual2.Id, "id match saved")
-	assert.Equal(t, user2.Email, actual2.Email, "email match expected")
-	assert.Equal(t, user2.Bio, actual2.Bio, "bio match expected")
-}
-
-func TestUserStorage_Users(t *testing.T) {
-	storage := New()
-
-	saved := make([]model.User, 0, 10)
-	for i := 1; i <= 10; i++ {
-		usr := genUser("test-mail"+strconv.Itoa(i), "test-bio"+strconv.Itoa(i))
-		s, err := storage.Save(context.Background(), usr.Email, usr.Bio)
-		require.NoError(t, err, "save should not error")
-
-		saved = append(saved, s)
-
-		users, err := storage.Users(context.Background())
-		require.NoError(t, err, "users should not error")
-		require.Len(t, users, i, "users len match expected")
-		for j := i; j > 0; j-- {
-			assert.Contains(t, users, saved[j-1], "users contains saved user")
-		}
-	}
+	})
 }
 
 func genUser(email string, bio string) *model.User {
